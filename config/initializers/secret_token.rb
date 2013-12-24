64a1bfa82a060d6ec0d515abda9e745c0275e637
darchcruise <darchcruise@yahoo.com>
2013-12-24 00:26:02 -0500

    login only for edit, update, and delete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mebay::Application.config.secret_token = '59c0b835c367057e19aa0f044e9819877daf9db449652b2e6c32a0c0ddef5168b050eb4c79652bc4da697538e6cf8bf4e47ade57135800acb0d6637284bc2cee'
